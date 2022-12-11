818fe41c5a9ebc3bec5752006f0b8c8bc3a7fdae
manideep <gembalimanideep@gmail.com>
2022-12-11 07:27:37 +0000

    solution

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+
+import './index.css'
+
+const DenominationItem = props => {
+  const {userDetails, decreaseAmount} = props
+  const {value} = userDetails
+  const onDecrease = () => {
+    decreaseAmount(value)
+  }
+  return (
+    <li>
+      <button className="buttin" type="button" onClick={onDecrease}>
+        {value}
+      </button>
+    </li>
+  )
+}
+export default DenominationItem
